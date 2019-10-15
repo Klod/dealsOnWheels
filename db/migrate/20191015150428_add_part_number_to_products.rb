2fa3e29ad873357dbe01e63459b0b9b07aa8a608
EC2 Default User <ec2-user@ip-172-31-47-13.us-east-2.compute.internal>
2019-10-15 19:44:33 +0000

    CommentOutProducts

--- db/migrate/20191015150428_add_part_number_to_products.rb
+++ db/migrate/20191015150428_add_part_number_to_products.rb
@@ -1,5 +1,5 @@
 class AddPartNumberToProducts < ActiveRecord::Migration[6.0]
   def change
-    add_column :products, :part_number, :string
+    #add_column :products, :part_number, :string
   end
 end
